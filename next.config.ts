72d660a68d59e8bd338c7163eb41a6dae679e200
Aditya Gupta <121934082+Adityagupta930@users.noreply.github.com>
2026-08-26 19:52:36 +0530

    chore: remove PWA configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  disable: process.env.NODE_ENV === "development",
-});
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.27"],
 };
 
-module.exports = withPWA(nextConfig);
+export default nextConfig;
